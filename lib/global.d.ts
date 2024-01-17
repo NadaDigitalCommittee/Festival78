13c7f345029f8b65fcd68dda7d3856c6e24566a1
altusershared <104960044+altusershared@users.noreply.github.com>
2024-01-17 10:14:21 +0900

    feat: fonts

---
+++ lib/global.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+declare namespace NodeJS {
+    interface ProcessEnv {
+      readonly NODE_ENV?: string;
+      readonly NEXT_PUBLIC_ADOBE_FONT_ID:string;
+    }
+  }
