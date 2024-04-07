6009c0ab95c08f7b7a41cafc02811846e23de0d4
inaenaro <bandongtailang223@gmail.com>
2024-04-07 14:44:10 +0900

    スマホ対応

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -28,23 +28,25 @@ export const HamburgerMenu: FC = () => {
   };
   return (
     <div className={`fixed z-50 right-0 ${isOpen ? "w-full h-screen backdrop-brightness-50" : ""}`}>
-      <button onClick={isOpen ? handleMenuClose : handleMenuOpen} className="absolute z-50 flex flex-col right-0 h-[72px] w-[72px] p-[20px] bg-theme justify-between items-center">
+      <button onClick={isOpen ? handleMenuClose : handleMenuOpen} className="absolute z-50 flex flex-col right-0 h-[4.5rem] w-[4.5rem] p-5 bg-theme justify-between items-center">
         <span
-          className={isOpen ? "block w-[32px] h-[4px] bg-white translate-y-[14px] rotate-[4.125turn] duration-300" : "block w-[32px] h-[4px] bg-white duration-300"}
+          className={`block h-1 bg-white duration-300 ${isOpen ? "w-10 translate-y-[0.875em] rotate-[4.125turn]" : "w-8"}`}
         />
         <span
-          className={isOpen ? "block w-[32px] h-[4px] opacity-0 bg-white duration-300" : "block w-[32px] h-[4px] bg-white duration-300"}
+          className={`block w-8 h-1 bg-white duration-300 ${isOpen ? "opacity-0" : ""}`}
         />
         <span
-          className={isOpen ? "block w-[32px] h-[4px] bg-white -translate-y-[14px] -rotate-[4.125turn] duration-300" : "block w-[32px] h-[4px] bg-white duration-300"}
+          className={`block h-1 bg-white duration-300 ${isOpen ? "w-10 -translate-y-[0.875em] -rotate-[4.125turn]" : "w-8"}`}
         />
       </button>
-      <div className={`absolute flex flex-col justify-between h-screen pb-12 pt-[52px] bg-theme duration-300 ${isOpen ? "right-0 w-[280px]" : "-right-24 w-0"}`}>
+      <div className={`absolute flex flex-col justify-between h-screen w-48 md:w-72 pt-[4.5rem] md:pt-8 pb-12 bg-theme duration-300 ${isOpen ? "right-0" : "-right-48 md:-right-72"}`}>
         {
           paths.map(({ ja, en, path }, i) => (
-            <div key={i} className="relative flex content-end left-auto r-0 h-12 px-12">
-              <a href={path} onMouseEnter={() => setHovered(i)} onMouseLeave={handleLeave} className="absolute bottom-0 font-zen_kaku_gothic_new text-xl text-white font-bold whitespace-nowrap">{ja}</a>
-              <div className={`absolute pl-[5%] pr-[20%] text-right font-south_amsterdam text-theme text-5xl whitespace-nowrap border-theme border-l-[16px] border-l-transparent ${hovered === i ? "border-b-2" : ""} ${isOpen ? "right-[100%]" : ""}`}>
+            <div key={i} className="relative flex h-[3.125rem] px-6 md:px-12">
+              <div className="flex z-40 items-center pb-[2px] font-zen_kaku_gothic_new text-xl text-white font-bold">
+                <a href={path} onMouseEnter={() => setHovered(i)} onMouseLeave={handleLeave}>{ja}</a>
+              </div>
+              <div className={`absolute flex z-30 items-center h-full pl-[5%] pr-[20%] text-right font-south_amsterdam text-theme text-[min(max(calc((100vw-15rem-1rem)/4.27),1rem),3rem)] md:text-5xl leading-none whitespace-nowrap border-theme border-l-[1rem] border-l-transparent border-b-2 transition-[right] duration-[600ms] ${hovered === i ? "border-b-theme" : "border-b-transparent"} ${isOpen ? "right-full" : "-right-full"}`}>
                 <a href={path} onMouseEnter={() => setHovered(i)} onMouseLeave={handleLeave}>{en}</a>
               </div>
             </div>
