81bdca874c83c708626916ea6348865fa891d9b7
Tito5e <tosirusan@gmail.com>
2024-04-14 19:41:54 +0900

    feat: add underline to title

--- components/events/ContentSection.tsx
+++ components/events/ContentSection.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import ContentLabelButton from "./ContentLabelButton";
 import styles from "@/styles/events.module.scss";
 
@@ -11,13 +12,13 @@ type ContentSectionProps = {
 }
 
 export default function ContentSection({title, eventId, content}: ContentSectionProps) {
-    return (<article id={`section_${eventId}`} className="pb-[100px]">
+    return (<article id={`section_${eventId}`}>
         <div className="flex justify-center">
-            <h1 className="pb-[30px] text-7xl font-zen_kaku_gothic_new font-bold">{title}</h1>
+            <h1 className={clsx("mb-[24px] pb-[24px] text-7xl font-zen_kaku_gothic_new font-bold", styles.stripeMarker)}>{title}</h1>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center pb-[100px]">
             <div className={styles.contentSection_wrapper}>
-                <div className="flex gap-[20px] justify-end">
+                <div className="flex gap-[20px] justify-end pb-[16px]">
                     <ContentLabelButton href="">
                         <span className="text-xl font-zen_kaku_gothic_new font-bold text-border">マップ
                             </span>
