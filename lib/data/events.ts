8a313d132901c9b2cc84014453a0127a90fb7f1c
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2024-04-21 00:52:25 +0900

    ビンゴ

--- lib/data/events.ts
+++ lib/data/events.ts
@@ -164,7 +164,7 @@ export const events: Event[] = [
   {
     id: "casino_event",
     name: "ビンゴ大会",
-    description: "...",
+    description: "今年も陸上部カジノにてビンゴ大会を行います！皆さんが欲しかったあの灘校グッズなどなど豪華景品が当たるかも？！ぜひお楽しみに！",
     category: "others",
     mapId: "h1-1",
   },
