19763745c9196811027e3142178787fcd8f00ec1
inaenaro <bandongtailang223@gmail.com>
2024-04-22 09:40:34 +0900

    文章更新

--- app/goods/page.tsx
+++ app/goods/page.tsx
@@ -17,12 +17,12 @@ import {
   ModalCloseButton
 } from "@chakra-ui/modal";
 import useEmblaCarousel from "embla-carousel-react";
-import { DotButton, useDotButton } from '@/components/EmblaDot';
 import {
   PrevButton,
   NextButton,
-  usePrevNextButtons
-} from '@/components/EmblaArrow';
+  usePrevNextButtons,
+  useDotButton
+} from '@/components/Embla';
 
 type Item = {
   id: number[],
@@ -38,42 +38,42 @@ const goods: Item[] = [{
   name: 'ルービック\nキューブ',
   imageCount: 2,
   price: 1700,
-  description: '台座付。インテリアにも\n正規ルービックキューブライセンスマーク入り'
+  description: '台座付 インテリアにも\n正規ルービックキューブライセンスマーク入'
 }, {
   id: [2],
   size: 'l',
   name: 'マグカップ',
   imageCount: 2,
   price: 1000,
-  description: '文化祭ロゴマグカップ。食洗機可。日本製\n280ml'
+  description: '280ml\n文化祭ロゴマグカップ 食洗機可 日本製'
 }, {
   id: [3],
   size: 'l',
   name: 'マフラー\nタオル',
   imageCount: 2,
   price: 1000,
-  description: '柔らかく品質のいいタオル。綿100%\n110×20cm'
+  description: '綿100% 110×20cm\n柔らかく品質のいいタオル'
 }, {
   id: [4],
   size: 'm',
   name: 'トートバッグ\n キャンバス地',
   imageCount: 1,
   price: 800,
-  description: 'ゆったりサイズ丈夫なキャンパス地\n横42×高さ38×幅13cm'
+  description: '横42×高さ38×幅13cm\nゆったりサイズ丈夫なキャンパス地'
 }, {
   id: [5],
   size: 'l',
   name: 'アクリル\nキーホルダー',
   imageCount: 1,
   price: 300,
-  description: '300円文化祭ロゴ。\n縦約5㎝'
+  description: '縦約5cm\n文化祭ロゴのキーホルダー'
 }, {
   id: [6],
   size: 'm',
   name: 'クリアしおり\n 2枚組',
   imageCount: 1,
   price: 100,
-  description: '丈夫なプラスチック素材。透明感がかっこい。\n135×47mm'
+  description: '135×47mm\n丈夫なプラスチック素材 透明感がかっこいい'
 }, {
   id: [7],
   size: 'l',
@@ -87,49 +87,49 @@ const goods: Item[] = [{
   name: 'Tシャツ',
   imageCount: 7,
   price: 1500,
-  description: 'サイズ S、M、L、LL'
+  description: 'サイズ S・M・L・LL'
 }, {
   id: [16],
   size: 's',
   name: 'ホットマン\nシェニール織り\nハンカチ',
   imageCount: 2,
   price: 2500,
-  description: '校門と本館の風景を芸術性の高いシェニール織りで表現。\n28×28cm'
+  description: '28×28cm\n校門と本館の風景を芸術性の高いシェニール織りで表現'
 }, {
   id: [17, 18, 19, 20],
   size: 'l',
   name: '白竹堂製\n校是布扇子',
   imageCount: 6,
   price: 1300,
-  description: '紺、えんじ、深緑、白地\n灘校書道教師倉橋先生による校是書字。綿製'
+  description: '紺・えんじ・深緑・白地 綿製\n灘校書道教師倉橋先生による校是書字'
 }, {
   id: [21, 22],
   size: 'l',
   name: '校章チャーム',
   imageCount: 1,
   price: 1000,
-  description: 'ダークブラウン、紺\n本革製校章チャーム。丁寧な手作りチャーム\n約3.3×3.3cm'
+  description: 'ダークブラウン・紺 約3.3×3.3cm\n本革製校章チャーム 丁寧な手作りチャーム'
 }, {
   id: [23],
   size: 'l',
   name: '灘ベア大\n(ぬいぐるみ)',
   imageCount: 3,
   price: 2300,
-  description: '約23cm\n灘校オリジナルオーダーのベアぬいぐるみ。NADAパーカー着用。背中に校章。'
+  description: '約23cm\n灘校オリジナルオーダーのベアぬいぐるみ NADAパーカー着用 背中に校章'
 }, {
   id: [24, 25],
   size: 'l',
   name: '灘ベア小\n キーホルダー',
   imageCount: 2,
   price: 800,
-  description: '約8cm\n紺、えんじ\n灘校オリジナルオーダーのベアストラップ。'
+  description: '紺・えんじ 約8cm\n灘校オリジナルオーダーのベアストラップ'
 }, {
   id: [26],
   size: 'm',
   name: 'ワンタッチ\nファイルケース',
   imageCount: 3,
   price: 300,
-  description: 'A4クリアファイルがそのまま入る。プリントをすっきり整理\n32.4×23cm\n収容枚数 コピー用紙60枚'
+  description: '32.4×23cm\n収容枚数 コピー用紙60枚\nA4クリアファイルがそのまま入る プリントをすっきり整理'
 }, {
   id: [27],
   size: 's',
@@ -157,49 +157,49 @@ const goods: Item[] = [{
   name: '三菱鉛筆\nユニスター B\n 6本組',
   imageCount: 1,
   price: 400,
-  description: 'ユニスターＢ'
+  description: ''
 }, {
   id: [35, 36, 37],
   size: 's',
   name: 'スマッシュ\n高機能\nシャープペン',
   imageCount: 3,
   price: 900,
-  description: 'ブラック、レッド、ダークグレー\n灘校生に大人気のスマッシュが灘校グッズに新登場'
+  description: 'ブラック・レッド・ダークグレー\n灘校生に大人気のスマッシュが灘校グッズに新登場'
 }, {
   id: [38, 39, 40],
   size: 'm',
   name: 'クルトガ KS\nシャープペン',
   imageCount: 3,
   price: 500,
-  description: 'ネイビー、 アイスブルー、ライトグレー\nクルトガ大人気モデル'
+  description: 'ネイビー・アイスブルー・ライトグレー\nクルトガ大人気モデル'
 }, {
   id: [41, 42, 43, 44],
   size: 'm',
   name: 'シャープペン\nベリーシャ楽',
   imageCount: 2,
   price: 100,
-  description: '黒、緑、ピンク、青\nお手頃価格だけど書き味抜群'
+  description: '黒・緑・ピンク・青\nお手頃価格だけど書き味抜群'
 }, {
   id: [45, 46, 47],
   size: 's',
   name: '3色ボール\nペン 0.5 ジェット\nストリーム',
   imageCount: 2,
   price: 500,
-  description: 'ブラック、ブルーグレー、アイスブルー\n間違えずに黒を出せる'
+  description: 'ブラック・ブルーグレー・アイスブルー\n間違えずに黒を出せる'
 }, {
   id: [48, 49, 50],
   size: 'm',
   name: '単色\nボールペン 0.5',
   imageCount: 2,
   price: 150,
-  description: 'スイスイ書ける単色ゲルインクボールペン。黒・赤・青'
+  description: '黒・赤・青\nスイスイ書ける単色ゲルインクボールペン'
 }, {
   id: [51, 52, 53, 54],
   size: 's',
   name: '多機能ペン\n ジェット\nストリーム\n',
   imageCount: 2,
   price: 700,
-  description: 'ネイビー、ボルドー、ブラック、ピンク\n最も使用頻度の高い赤・黒ボールペン＆シャープペンシルの組み合わせ'
+  description: 'ネイビー・ボルドー・ブラック・ピンク\n最も使用頻度の高い赤・黒ボールペン&シャープペンシルの組み合わせ'
 }, {
   id: [55, 56, 57, 58],
   size: 'm',
@@ -213,7 +213,7 @@ const goods: Item[] = [{
   name: '蛍光ペン\n 3本組\n 灘校章ケース入',
   imageCount: 1,
   price: 400,
-  description: '灘校校章ケース入り'
+  description: ''
 }, {
   id: [60],
   size: 'l',
@@ -241,7 +241,7 @@ const goods: Item[] = [{
   name: '不織布バッグ',
   imageCount: 2,
   price: 100,
-  description: '横270×高さ345×幅60mm\n＊無料レジ袋にかわるエコバッグの位置づけ。\n1-63の商品購入者のみ購入可'
+  description: '横270×高さ345×幅60mm\n※無料レジ袋に代わるエコバッグの位置づけ\n1-63の商品購入者のみ購入可'
 }];
 const souvenirs: Item[] = [{
   id: [1],
@@ -391,35 +391,37 @@ const Frame: FC<{ isGoods?: boolean, item: Item; isLarge?: boolean }> = ({ isGoo
             <ModalContent>
               <ModalHeader>
                 <p className="text-theme text-2xl">{id.length === 1 ? id : `${id[0]}-${id[id.length - 1]}`}. {name.split('\n').join('')}</p>
-                <p>{description.split('\n').map((x, i) => (<span key={i} className="block">{x}</span>))}</p>
+                <p className="px-2">{description.split('\n').map((x, i) => (<span key={i} className="block">{x}</span>))}</p>
               </ModalHeader>
               <ModalCloseButton className="text-theme" />
               <ModalBody>
                 <section className="embla max-w-[48rem] m-auto rounded-xl [--slide-height:19rem] [--slide-spacing:1rem] [--slide-size:80%]">
                   <div className="embla__viewport overflow-hidden" ref={emblaRef}>
                     <div className="embla__container [backface-visibility:hidden] flex touch-pan-y ml-[calc(var(--slide-spacing)*-1)]">
-                      {Array.from(Array(imageCount).keys()).map(i=>(<div className="embla__slide flex-[0_0_var(--slide-size)] min-w-0 aspect-square pl-[var(--slide-spacing)]" key={i}>
-                        <div className="relative size-full p-1 border-2 border-gray-200 rounded-3xl">
-                          <Image src={`/img/items/${isGoods ? "" : "souvenirs/"}${`0${id[0]}`.slice(-2)}_${i+1}.jpg`} alt={name} fill className="object-contain" />
+                      {Array.from(Array(imageCount).keys()).map(i => (<div className="embla__slide flex-[0_0_var(--slide-size)] min-w-0 aspect-square pl-[var(--slide-spacing)]" key={i}>
+                        <div className="relative aspect-square border-2 border-theme/50">
+                          <Image src={`/img/items/${isGoods ? "" : "souvenirs/"}${`0${id[0]}`.slice(-2)}_${i + 1}.jpg`} alt={name} fill className="object-contain" />
                         </div>
                       </div>))}
                     </div>
                   </div>
-                  <div className="embla__controls grid-cols-[auto_1fr] justify-between gap-[1.2rem] mt-[1.8rem]">
-                    <div className="embla__buttons grid grid-cols-2 gap-[1.6rem] items-center">
+                  <div className="embla__controls flex justify-between mt-[1.8rem]">
+                    <div className="items-center text-theme">
                       <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-                      <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
                     </div>
                     <div className="embla__dots flex flex-wrap justify-end items-center mr-[calc((2.6rem-1.4rem)/2*(-1))]">
                       {scrollSnaps.map((_, index) => (
-                        <DotButton
+                        <button
                           key={index}
                           onClick={() => onDotButtonClick(index)}
-                          className={'embla__dot'.concat(
-                            index === selectedIndex ? ' embla__dot--selected' : ''
+                          className={'embla__dot flex items-center justify-center size-[2.6rem] rounded-[50%] after:shadow-[inset_0_0_0_0.2rem_rgb(234,234,234)] after:flex after:size-[1.4rem] after:rounded-[50%] after:items-center]'.concat(
+                            index === selectedIndex ? ' after:shadow-theme after:shadow-[inset_0_0_0_0.2rem]' : ''
                           )}
                         />
                       ))}
+                    </div>
+                    <div className="items-center text-theme">
+                      <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
                     </div>
                   </div>
                 </section>
