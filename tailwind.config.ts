7eafb14112c90e3f696d617a9fc84facc15a49f8
Ghaaj <gaerobge1729@gmail.com>
2024-04-29 10:05:46 +0900

    fix circles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,21 @@
 import type { Config } from "tailwindcss";
 
+export const PlaceLabelBgColors = {
+  East: "[#2DD4DE]",
+  G1: "[#27B82D]",
+  Gym: "[#2BE127]",
+  H1: "[#3C2E90]",
+  H2: "[#454AD3]",
+  H3: "[#518ADF]",
+  H4: "[#73AFF6]",
+  Kensyu: "[#FFB82D]",
+  Kyoei: "[#D227E1]",
+  M2: "[#D34545]",
+  M3: "[#EA536F]",
+  M4: "[#F26DC4]",
+  NewGarden: "[#98580D]",
+};
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -26,5 +42,6 @@ const config: Config = {
     },
   },
   plugins: [],
+  safelist: [...Object.values(PlaceLabelBgColors).map((c) => `bg-${c}`)],
 };
 export default config;
