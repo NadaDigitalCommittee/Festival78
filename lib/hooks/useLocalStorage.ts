0465356ccd653f31c86e3c373aa12fbbd2c5c21f
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2024-05-01 12:18:10 +0900

    fix

--- lib/hooks/useLocalStorage.ts
+++ lib/hooks/useLocalStorage.ts
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 
 export const useLocalStorage = <T>(key: string, initialValue: T) => {
   const [storedValue, setStoredValue] = useState<T>(() => {
+    if(typeof window==="undefined"){
+      return initialValue;
+    }
     const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : initialValue;
   });
